3346cf8254f0c6473ba8b877a416cc95c4d89ee1
WilliamEduardo20 <williameduardominorelli@gmail.com>
2026-04-23 16:49:20 -0300

    23/04/2026 16:49
    Menu novo.

--- script.js
+++ script.js
@@ -2,17 +2,3 @@ document.getElementById("menu-button").addEventListener("click", function() {
     var menu = document.getElementById("menu");
     menu.classList.toggle("closed");
 });
-
-document.getElementById("html").addEventListener("click", function() {
-    window.location.href = "index.html";
-});
-document.getElementById("nav-list-html").addEventListener("click", function() {
-    window.location.href = "index.html";
-});
-
-document.getElementById("css").addEventListener("click", function() {
-    window.location.href = "inde.html";
-});
-document.getElementById("nav-list-css").addEventListener("click", function() {
-    window.location.href = "inde.html";
-});
